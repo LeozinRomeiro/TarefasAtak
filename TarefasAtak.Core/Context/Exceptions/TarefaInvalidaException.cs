0ebd5a7e1a45ec7c360fe0f2a540fcd377580931
Leonardo Romeiro <leozinwork@gmail.com>
2024-04-19 20:36:32 -0300

    Criando excesões para caso de descrisão vazia

---
+++ TarefasAtak.Core/Context/Exceptions/TarefaInvalidaException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TarefasAtak.Core.Context.Exceptions
+{
+    public class TarefaInvalidaException : Exception
+    {
+        private const string MessagemPadrao = "Tarefa invalida";
+
+        public TarefaInvalidaException(string message = MessagemPadrao)
+            : base(message)
+        {
+        }
+
+        public static void ThrowIfInvalid(string texto, string mensagem = MessagemPadrao)
+        {
+            if (string.IsNullOrEmpty(texto))
+                throw new TarefaInvalidaException(mensagem);
+        }
+    }
+}
