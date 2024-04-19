d978c0e1ede52219b11311eaa00b1db749d704bc
Leonardo Romeiro <leozinwork@gmail.com>
2024-04-19 09:53:24 -0300

    Criando classe tarefa

---
+++ TarefasAtak.Core/Entities/Tarefa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TarefasAtak.Core.Entities
+{
+    public class Tarefa
+    {
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+    }
+}
