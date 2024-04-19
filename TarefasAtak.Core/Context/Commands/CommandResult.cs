796a8452173e393dd3daef311e1d7deb3bf81e60
Leonardo Romeiro <leozinwork@gmail.com>
2024-04-19 13:00:05 -0300

    Definindo atributos do comando de resultado e finalizando manibulação de criação de tarefa

--- TarefasAtak.Core/Context/Commands/CommandResult.cs
+++ TarefasAtak.Core/Context/Commands/CommandResult.cs
@@ -9,5 +9,15 @@ namespace TarefasAtak.Core.Context.Commands
 {
     public class CommandResult : ICommandResult
     {
+        public CommandResult(bool success, string message, object data)
+        {
+            Success = success;
+            Message = message;
+            Data = data;
+        }
+
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public object Data { get; set; }
     }
 }
