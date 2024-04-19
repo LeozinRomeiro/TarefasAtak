04357bceef7f5dbad53323b69b0ad1d1aca68907
Leonardo Romeiro <leozinwork@gmail.com>
2024-04-19 10:27:51 -0300

    Adicionando atributos necessarios para a classe

--- TarefasAtak.Core/Context/Entities/Tarefa.cs
+++ TarefasAtak.Core/Context/Entities/Tarefa.cs
@@ -3,12 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TarefasAtak.Core.Context.Enums;
 
 namespace TarefasAtak.Core.Context.Entities
 {
     public class Tarefa
     {
+        public Tarefa(string titulo, string descricao, Status status)
+        {
+            Titulo = titulo;
+            Descricao = descricao;
+            Status = status;
+        }
+
         public string Titulo { get; set; }
         public string Descricao { get; set; }
+        public Status Status { get; set; }
     }
 }
---
+++ TarefasAtak.Core/Context/Enums/Status.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TarefasAtak.Core.Context.Enums
+{
+    public enum Status
+    {
+        Nova = 0 ,
+        Andamento = 1,
+        Completa = 2,
+    }
+}
