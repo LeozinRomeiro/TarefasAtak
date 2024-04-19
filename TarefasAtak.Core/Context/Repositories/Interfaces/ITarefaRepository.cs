64e539e2e08e60c65f8c0121b185f485a70ff17a
Leonardo Romeiro <leozinwork@gmail.com>
2024-04-19 11:09:24 -0300

    Abstraindo o repositorio

---
+++ TarefasAtak.Core/Context/Repositories/Interfaces/ITarefaRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TarefasAtak.Core.Context.Entities;
+
+namespace TarefasAtak.Core.Context.Repositories.Interfaces
+{
+    public interface ITarefaRepository
+    {
+        public void Save(Tarefa tarefa);
+    }
+}
