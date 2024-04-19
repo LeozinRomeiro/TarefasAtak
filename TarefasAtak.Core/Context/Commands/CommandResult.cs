95aef6bf3a9295f4ca5b5f3ff2fc6be7717e91a0
Leonardo Romeiro <leozinwork@gmail.com>
2024-04-19 12:36:15 -0300

    Comando generico de resultado

---
+++ TarefasAtak.Core/Context/Commands/CommandResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TarefasAtak.Core.Context.Commands.Interfaces;
+
+namespace TarefasAtak.Core.Context.Commands
+{
+    public class CommandResult : ICommandResult
+    {
+    }
+}
---
+++ TarefasAtak.Core/Context/Commands/Interfaces/ICommandResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TarefasAtak.Core.Context.Commands.Interfaces
+{
+    public interface ICommandResult
+    {
+    }
+}
