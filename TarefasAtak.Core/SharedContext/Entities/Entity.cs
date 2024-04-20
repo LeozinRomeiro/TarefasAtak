f552b04d4f68c5a6136a3f64869fef2c16b3d58f
Leonardo Romeiro <leozinwork@gmail.com>
2024-04-19 22:33:52 -0300

    Definindo chave de identificação na classe pai entity

---
+++ TarefasAtak.Core/SharedContext/Entities/Entity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TarefasAtak.Core.SharedContext.Entities
+{
+    public abstract class Entity : IEquatable<Guid>
+    {
+        protected Entity() => Id = Guid.NewGuid();
+
+        public Guid Id { get; }
+        public bool Equals(Guid id) => Id == id;
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
