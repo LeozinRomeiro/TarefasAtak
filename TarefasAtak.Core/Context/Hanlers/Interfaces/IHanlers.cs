3611814ba9bda8d3d09f6e17456c5c397b1f7ebb
Leonardo Romeiro <leozinwork@gmail.com>
2024-04-19 22:33:10 -0300

    Corrigindo interface

--- TarefasAtak.Core/Context/Hanlers/Interfaces/IHanlers.cs
+++ TarefasAtak.Core/Context/Hanlers/Interfaces/IHanlers.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Input;
+using TarefasAtak.Core.Context.Commands.Interfaces;
 
 namespace TarefasAtak.Core.Context.Hanlers.Interfaces
 {
