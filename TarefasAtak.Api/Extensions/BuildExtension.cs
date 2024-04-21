ecffe6846ff045188377a6b7dcbad40c716d2d08
Leonardo Romeiro <leozinwork@gmail.com>
2024-04-20 22:17:38 -0300

    Criando extensão de build para organizar program

---
+++ TarefasAtak.Api/Extensions/BuildExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using TarefasAtak.Core.Context.Entities;
+using TarefasAtak.Core.Context.Repositories.Interfaces;
+using TarefasAtak.Core.Context.Servicos;
+using TarefasAtak.Core.Context.Servicos.Interfaces;
+using TarefasAtak.Infra.Context.Repositories;
+using TarefasAtak.Infra.Data;
+
+namespace TarefasAtak.Api.Extensions
+{
+    public static class BuildExtension
+    {
+        public static void AddTarefaContext(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddScoped<ITarefaRepository, TarefaRepository>();
+            builder.Services.AddScoped<ITarefaServico, TarefaServico>();
+        }
+        public static void AddConfiguration(this WebApplicationBuilder builder)
+        {
+
+        }
+        public static void AddDatabase(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddScoped(x=>new AppDbContext<Tarefa>());
+        }
+
+    }
+}
