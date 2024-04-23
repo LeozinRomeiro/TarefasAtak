5dc0a6859fd6311f66a27c5e14dc8331a251edf5
Leonardo Romeiro <leozinwork@gmail.com>
2024-04-23 13:48:42 -0300

    ModelDTos desnecessario , simplicando codigo

---
+++ TarefasAtak.Api/Mappers/DtoToModelMappingTarefaCommand.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using TarefasAtak.Core.Context.Commands;
+using TarefasAtak.Core.Context.Entities;
+
+namespace TarefasAtak.App.Mappers
+{
+    public class DtoToModelMappingTarefaCommand : Profile
+    {
+        public DtoToModelMappingTarefaCommand()
+        {
+            TarefaMap();
+        }
+
+        private void TarefaMap()
+        {
+            CreateMap<Tarefa, TarefaCommand>()
+                .ForMember(e=>e.Id, d=>d.MapFrom(x=>x.Id))
+                .ForMember(e => e.Titulo, d => d.MapFrom(x => x.Titulo))
+                .ForPath(dest => dest.Descricao, opt => opt.MapFrom(src => src.Descricao.Texto))
+                .ForMember(e => e.Status, d => d.MapFrom(x => x.Status));
+        }
+    }
+}
