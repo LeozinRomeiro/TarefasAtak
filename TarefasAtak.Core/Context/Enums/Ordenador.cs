4f26b0f53cba2af9b5a3fc979f1ec8829d635c29
Leonardo Romeiro <leozinwork@gmail.com>
2024-04-23 22:29:44 -0300

    Ordenação funcionando.

---
+++ TarefasAtak.Core/Context/Enums/Ordenador.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TarefasAtak.Core.Context.Enums
+{
+    public enum Ordenador
+    {
+        Titulo = 0,
+        Descricao = 1,
+        Status = 2,
+    }
+
+}
